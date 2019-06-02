a7dbc13e90943cabfb15651b12ff1b412146879c
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2019-06-02 16:58:11 +0100

    Update TiersOfConversion.sol

--- TiersOfConversion.sol
+++ TiersOfConversion.sol
@@ -1,5 +1,6 @@
 contract TiersOfConversion {
     
+    // Defines the parameters to be kept track of for each user
     struct User {
         uint8 tier;
         uint numberOfTransactions;
@@ -10,17 +11,33 @@ contract TiersOfConversion {
     
     mapping(address => User) users;
     
+    // ARBITRARY VALUES FOR EXAMPLE ONLY
     uint constant maxTransactions = 10000;
     uint constant maxAddresses = 1000;
     uint constant maxTime = 730 days;
-
+    
+    // Tier calculated after every transaction --> not efficient, model must be improved
+    // Suggestion: wallet keeps track of tier off-chain and calls to update once as the tier increases
     function calculateTier(address update) internal {
             uint8 newTier = 0;
+            
+            // Checks current time vs. time user joined system
             users[update].timeInPlatform = block.timestamp - users[update].joinedTimestamp;
+            
+            // Sets score for n(transactions)
             uint nPctg = uint(100*users[update].numberOfTransactions / maxTransactions);
+            
+            // Sets score for distinct addresses
             uint addressesPctg = uint(100*users[update].numberOfUniqueAddresses / maxAddresses);
+                      
+                      
+            // Sets score for time
             uint timePctg = uint(100*users[update].timeInPlatform / maxTime);
+            
+            // Gets score out of 100
             uint score = 100*(nPctg + addressesPctg + timePctg)/ 300;
+            
+            // Maps score to tier
             if(score < 20) {
                 newTier = 1;
             }
@@ -36,6 +53,8 @@ contract TiersOfConversion {
             else {
                 newTier = 5;
             }
+            
+            // Updates tier
             users[update].tier = newTier;
     }
     
