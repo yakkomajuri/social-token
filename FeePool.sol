8ee8b5c1da008ebb70091eb8b18726eeb2cab355
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2019-06-02 16:46:24 +0100

    Update FeePool.sol

--- FeePool.sol
+++ FeePool.sol
@@ -1,18 +1,29 @@
 pragma solidity^0.5.0;
 
+// Defines SocialToken contract to allow calls
 contract SocialToken {
+       
+       // Checks if an address is whitelisted in the SocialToken system
        function isWhitelisted(address _ad) external view returns(bool) { }
 }
 
+// A pool of funds to cover transaction fees of users in the SocialToken system
 contract FeePool {
     
-    
+    //Substitute for address of SocialToken
     SocialToken tokenContract = SocialToken([ADDRESS HERE]);
     
-    function getEther() public {
+   // Sends ETH to wallet requesting funds
+   function getEther() public {
+   
+        // Wallet must be whitelisted
         require(tokenContract.isWhitelisted(msg.sender));
+        
+        // Balance must be low enough, ideally a function would exist to set the threshold
         require(msg.sender.balance < 0.01 ether);
-        msg.sender.transfer(0.01 ether);
+        
+        // Transfer funds to wallet
+        msg.sender.transfer(0.05 ether);
     }
     
 }
