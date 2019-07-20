f8dfb869dda4dab465b585fef49a2dfb9506bf52
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2019-07-19 23:37:05 -0300

    Update TiersOfConversion.sol

--- TiersOfConversion.sol
+++ TiersOfConversion.sol
@@ -2,6 +2,7 @@ pragma solidity^0.5.0;
 
 contract TiersOfConversion {
     
+    // Defines the parameters to be kept track of for each user
     struct User {
         uint8 tier;
         uint numberOfTransactions;
@@ -15,14 +16,28 @@ contract TiersOfConversion {
     uint maxTransactions;
     uint maxAddresses;
     uint maxTime;
-
+    
+    // Tier calculated after every transaction --> not efficient, model must be improved
+    // Suggestion: wallet keeps track of tier off-chain and calls to update once as the tier increases
     function calculateTier(address update) internal {
             uint8 newTier = 0;
+            
+            // Checks current time vs. time user joined system
             users[update].timeInPlatform = block.timestamp - users[update].joinedTimestamp;
+            
+            // Sets score for n(transactions)
             uint nPctg = uint(100*users[update].numberOfTransactions / maxTransactions);
+            
+            // Sets score for distinct addresses
             uint addressesPctg = uint(100*users[update].numberOfUniqueAddresses / maxAddresses);
+            
+            // Sets score for time
             uint timePctg = uint(100*users[update].timeInPlatform / maxTime);
+            
+            // Gets score out of 100
             uint score = 100*(nPctg + addressesPctg + timePctg)/ 300;
+            
+            // Maps score to tier
             if(score < 20) {
                 newTier = 1;
             }
@@ -38,6 +53,8 @@ contract TiersOfConversion {
             else {
                 newTier = 5;
             }
+            
+            // Updates tier
             users[update].tier = newTier;
     }
     
